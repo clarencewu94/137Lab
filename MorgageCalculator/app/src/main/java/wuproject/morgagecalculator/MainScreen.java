a4494144cae6757ececdcda4a1160ea26f039c1f
Clarencewu94 <Clarencewu94@yahoo.com>
2017-05-09 16:30:05 -0700

    New stuff

--- MorgageCalculator/app/src/main/java/wuproject/morgagecalculator/MainScreen.java
+++ MorgageCalculator/app/src/main/java/wuproject/morgagecalculator/MainScreen.java
@@ -2,28 +2,31 @@ package wuproject.morgagecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainScreen extends AppCompatActivity {
 
-    private static final String Home_Value = "Home Value";
-    private static final String Interest_Rate = " Interest Rate";
+    private static final String Home_Value = "Home_Value";
+    private static final String Down_Payment = "Down_Payment";
+    private static final String Interest_Rate = " Interest_Rate";
+    private static final String Property_tax = "Property_tax";
+
 
     //Amount being inserted
-    private double HomeInput;//user input
-    private double InterestInput;
+    private double HomeValue;//user input
+    private double DownPayment;
+    private double InterestRate;
+    private int PropertyTaxes;
     //Accepts User Input
-    private EditText HomeInput; //user input for Home Payment
+    private EditText homeInput; //user input for Home Payment
     private EditText DownInput; //user input for Down Payment
-
-    private EditText InterestRate; // custom rate
+    private EditText InterestInput;
+    private EditText PropertyInput; // custom rate
 
     //amount of terms
     private EditText FirstTerm; // 15 yr monthly
-    private EditText SecondTerm; // 20 yr monthly
-    private EditText ThirdTerm;  // 25 yr monthly
-    private EditText FourthTerm; //30 yr monthly
-    private EditText FifthTerm;  //35 yr montly
     //declaring the amount of terms
 
 
@@ -32,25 +35,64 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        if ( savedInstanceState == null ) // the app just started running
+        if(savedInstanceState == null){
+            HomeValue = 0.0;
+            DownPayment = 0.0;
+            InterestRate = 0.0;
+            PropertyTaxes = 0;
+        }
+        else
         {
-            HomeInput = 0.0; // initialize the loan amount to zero
-        } // end if
-        else // app is being restored from memory, not executed from scratch
-        {
-            // initialize the loan amount to saved amount
-            HomeInput = savedInstanceState.getDouble(Home_Value);
+            HomeValue = savedInstanceState.getDouble(Home_Value);
+            DownPayment = savedInstanceState.getDouble(Down_Payment);
+            InterestRate = savedInstanceState.getDouble(Interest_Rate);
+            PropertyTaxes = savedInstanceState.getInt(Property_tax);
 
-            // initialize the custom rate to saved interest rate
+        }
 
-        } // end else
+        homeInput = (EditText)findViewById(R.id.homeInput);
+        DownInput = (EditText)findViewById(R.id.DownInput);
+        InterestInput = (EditText)findViewById(R.id.InterestInput);
+        PropertyInput = (EditText)findViewById(R.id.PropertyInput);
 
-        Interest_Rate = (TextView) findViewById(R.id.InterestRate);
-        loanEditText = (EditText) findViewById(R.id.loanEditText);
+        Button Calculate = (Button)findViewById(R.id.Calculate);
+        Calculate.setOnClickListener(CalculateListener);
+        Button Reset = (Button)findViewById(R.id.Reset);
+        Reset.setOnClickListener(ResetListener);
+
 
 
     }
+    public View.OnClickListener CalculateListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ((InputMethodManager) getSystemService(
+                    Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+                    housePriceEditText.getWindowToken(), 0);
 
+            Calculate();
+        }
+    };
+    public View.OnClickListener cancelButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // hide the soft keyboard
+            ((InputMethodManager) getSystemService(
+                    Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+                    housePriceEditText.getWindowToken(), 0);
+
+            Reset();
+        }
+    };
+    private void Reset(){
+
+        //reset every field
+        homeInput.setText(String.format("0.0"));// Is this right??
+        DownInput.setText("0.0");
+        InterestInput.setText("0.0");
+        PropertyInput.setText("0");
+
+    }
     // calculate monthly payment
     private double formula(double loan, double rate, int term)
     {
@@ -63,21 +105,11 @@ public class MainScreen extends AppCompatActivity {
     {
         // calculate monthly payment
         double FirstTerm =
-                formula(HomeInput,InterestRate, 15 );
-        double SecondTerm =
-                formula(HomeInput,InterestRate, 20 );
-        double ThirdTerm =
-                formula(HomeInput,InterestRate, 25 );
-        double ForthTerm =
-                formula(HomeInput,InterestRate, 30 );
-        double FifthTerm =
-                formula(HomeInput,InterestRate, 35 );
+                formula(homeInput,InterestRate, 15);
+
         // 10, 15 and 30 yr monthly payment EditTexts
         FirstTerm.setText("$" + String.format("%.0f", FirstTerm));
-        SecondTerm.setText("$" + String.format("%.0f", SecondTerm));
-        ThirdTerm.setText("$" + String.format("%.0f", ThirdTerm));
-        ForthTerm.setText("$" + String.format("%.0f", ForthTerm));
-        FifthTerm.setText("$" + String.format("%.0f", FifthTerm));
+
     }
 
 
